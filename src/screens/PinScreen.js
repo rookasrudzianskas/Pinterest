6bcde1694a9d1acc858e893b6d71748280a0756c
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 21:17:02 +0300

    Rokas

---
+++ src/screens/PinScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const PinScreen = () => {
+    return (
+        <View>
+            <Text className="text-white">
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default PinScreen;
