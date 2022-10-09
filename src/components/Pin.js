ab25873976e4ee78405737d0f4e897be0b6537fe
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 17:25:50 +0300

    Rokas

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -9,10 +9,12 @@ const Pin = ({pin: {id, title, image}}) => {
 
     return (
         <View style={styles.pin}>
-            <Image style={styles.image} className="relative" source={{uri: image}} />
-            <TouchableOpacity className="absolute bg-[#D3CFD4] rounded-full p-1 bottom-[60px] right-[20px]" onPres={onLine} activeOpacity={0.7}>
-                <AntDesign name="hearto" size={22} color="black" />
-            </TouchableOpacity>
+            <View className="relative">
+                <Image style={styles.image} className="" source={{uri: image}} />
+                <TouchableOpacity className="absolute bg-[#D3CFD4] rounded-full p-1 bottom-[10px] right-[13px]" onPres={onLine} activeOpacity={0.7}>
+                    <AntDesign name="hearto" size={17} color="black" />
+                </TouchableOpacity>
+            </View>
             <Text className="font-bold text-[18px] m-3 text-black">{title}</Text>
         </View>
     );
