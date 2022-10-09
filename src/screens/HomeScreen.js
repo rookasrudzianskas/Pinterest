bdf08065a39351b6dad83215cb49cef0b2fc8338
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 17:07:00 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen =({ navigation }) => {
       <View className="flex-1" style={styles.container}>
         <View style={styles.pin}>
             <Image style={styles.image} className="" source={{uri: 'https://ih1.redbubble.net/image.966976056.3207/ssrco,mhoodiez,mens,101010:01c5ca27c6,front,square_product,600x600-bg,f8f8f8.jpg'}} />
-            <Text className="font-bold text-[18px] text-black">rookas Hoodie</Text>
+            <Text className="font-bold text-[18px] m-3 text-black">rookas Hoodie</Text>
         </View>
         <StatusBar style="auto" />
       </View>
