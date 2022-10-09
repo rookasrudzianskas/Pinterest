53a3baf118f09e67c232c1db5073164b8916c07f
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 21:00:47 +0300

    ROkas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,20 +11,15 @@ const HomeScreen =({ navigation }) => {
           <View className="flex-1" style={styles.container}>
               <View style={{backgroundColor: '#94ffa955', flex: 1}}>
                   <Pin pin={PinsData[0]} />
-                  <Pin pin={PinsData[1]} />
                   <Pin pin={PinsData[2]} />
-                  <Pin pin={PinsData[3]} />
                   <Pin pin={PinsData[4]} />
-                  <Pin pin={PinsData[5]} />
                   <Pin pin={PinsData[6]} />
               </View>
               <View style={{backgroundColor: '#94ffa9', flex: 1}}>
                   <Pin pin={PinsData[1]} />
-                  <Pin pin={PinsData[2]} />
                   <Pin pin={PinsData[3]} />
-                  <Pin pin={PinsData[4]} />
                   <Pin pin={PinsData[5]} />
-                  <Pin pin={PinsData[6]} />
+                  <Pin pin={PinsData[7]} />
               </View>
           </View>
       </ScrollView>
