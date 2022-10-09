ee826d8d8b33fa45c5ed3b2d700ca61ec27dce96
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 17:11:31 +0300

    Roaks

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,11 +3,14 @@ import {StatusBar} from "expo-status-bar";
 import {Image} from "react-native";
 import {StyleSheet} from "react-native";
 import Pin from "../components/Pin";
+import PinsData from '../assets/data/pins';
 
 const HomeScreen =({ navigation }) => {
   return (
       <View className="flex-1" style={styles.container}>
-          <Pin />
+          {PinsData.map((pin) => (
+              <Pin key={pin.id} image={pin.image} title={pin.title} />
+          ))}
         <StatusBar style="auto" />
       </View>
   );
