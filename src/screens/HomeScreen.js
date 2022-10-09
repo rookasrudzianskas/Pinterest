c6cbc871a53375fd93d93de0b162adc27158fcf5
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 17:08:11 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -10,6 +10,16 @@ const HomeScreen =({ navigation }) => {
             <Image style={styles.image} className="" source={{uri: 'https://ih1.redbubble.net/image.966976056.3207/ssrco,mhoodiez,mens,101010:01c5ca27c6,front,square_product,600x600-bg,f8f8f8.jpg'}} />
             <Text className="font-bold text-[18px] m-3 text-black">rookas Hoodie</Text>
         </View>
+
+          <View style={styles.pin}>
+              <Image style={styles.image} className="" source={{uri: 'https://ih1.redbubble.net/image.966976056.3207/ssrco,mhoodiez,mens,101010:01c5ca27c6,front,square_product,600x600-bg,f8f8f8.jpg'}} />
+              <Text className="font-bold text-[18px] m-3 text-black">rookas Hoodie</Text>
+          </View>
+
+          <View style={styles.pin}>
+              <Image style={styles.image} className="" source={{uri: 'https://ih1.redbubble.net/image.966976056.3207/ssrco,mhoodiez,mens,101010:01c5ca27c6,front,square_product,600x600-bg,f8f8f8.jpg'}} />
+              <Text className="font-bold text-[18px] m-3 text-black">rookas Hoodie</Text>
+          </View>
         <StatusBar style="auto" />
       </View>
   );
