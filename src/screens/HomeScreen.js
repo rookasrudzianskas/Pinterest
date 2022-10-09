b9844339a3eebb099b06164bfe0f6951374127c9
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 20:57:33 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,14 +7,27 @@ import PinsData from '../assets/data/pins';
 
 const HomeScreen =({ navigation }) => {
   return (
-      <View className="flex-1" style={styles.container}>
-          <View style={{backgroundColor: '#94ffa955', flex: 1}}>
-              <Pin pin={PinsData[0]} />
+      <ScrollView contentContainerStyle={{paddingBottom: 100}} showsVerticalScrollIndicator={false}>
+          <View className="flex-1" style={styles.container}>
+              <View style={{backgroundColor: '#94ffa955', flex: 1}}>
+                  <Pin pin={PinsData[0]} />
+                  <Pin pin={PinsData[1]} />
+                  <Pin pin={PinsData[2]} />
+                  <Pin pin={PinsData[3]} />
+                  <Pin pin={PinsData[4]} />
+                  <Pin pin={PinsData[5]} />
+                  <Pin pin={PinsData[6]} />
+              </View>
+              <View style={{backgroundColor: '#94ffa9', flex: 1}}>
+                  <Pin pin={PinsData[1]} />
+                  <Pin pin={PinsData[2]} />
+                  <Pin pin={PinsData[3]} />
+                  <Pin pin={PinsData[4]} />
+                  <Pin pin={PinsData[5]} />
+                  <Pin pin={PinsData[6]} />
+              </View>
           </View>
-          <View style={{backgroundColor: '#94ffa9', flex: 1}}>
-              <Pin pin={PinsData[1]} />
-          </View>
-      </View>
+      </ScrollView>
   );
 }
 
