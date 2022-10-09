298001db24c9b69d4914dc416ceb22519f47cd76
Rokas <rookas.rudzenskas@gmail.com>
2022-10-09 21:03:43 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -10,16 +10,10 @@ const HomeScreen =({ navigation }) => {
       <ScrollView contentContainerStyle={{paddingBottom: 100}} showsVerticalScrollIndicator={false}>
           <View className="flex-1" style={styles.container}>
               <View style={{backgroundColor: '#94ffa955', flex: 1}}>
-                  <Pin pin={PinsData[0]} />
-                  <Pin pin={PinsData[2]} />
-                  <Pin pin={PinsData[4]} />
-                  <Pin pin={PinsData[6]} />
+                  {PinsData.map((pin, index) => (<Pin key={pin.id} pin={pin} />))}
               </View>
               <View style={{backgroundColor: '#94ffa9', flex: 1}}>
-                  <Pin pin={PinsData[1]} />
-                  <Pin pin={PinsData[3]} />
-                  <Pin pin={PinsData[5]} />
-                  <Pin pin={PinsData[7]} />
+                  {PinsData.map((pin, index) => (<Pin key={pin.id} pin={pin} />))}
               </View>
           </View>
       </ScrollView>
