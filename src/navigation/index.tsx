cec237aa1f60f6058de09866649b9bb594983a81
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 13:10:26 +0300

    Rokas

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -83,10 +83,3 @@ function BottomTabNavigator() {
     </BottomTab.Navigator>
   );
 }
-
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
-}
