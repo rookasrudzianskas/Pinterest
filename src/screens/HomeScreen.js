d8eddb64a201f5fa09b1504874afe11252ee64f6
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 15:49:44 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,20 @@
 import PinsData from '../assets/data/pins';
 import MasonryList from "../components/MasonryList";
+import {useEffect} from "react";
 
 const HomeScreen =({ navigation }) => {
+    const fetchPins = async () => {
+        const { data, error } = await nhost.graphql.request(`
+            query { pins {
+                     id
+                     image
+                     created_at
+                     title
+                     user_id
+            } }
+        `); };
+    useEffect(() => { fetchPins(); }, []);
+
   return (
       <>
         <MasonryList pins={PinsData} />
