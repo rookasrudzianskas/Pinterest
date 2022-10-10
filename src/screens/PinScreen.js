425786b16619f6fa06b13ecfe0c630f3368c76a2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 16:18:36 +0300

    Rokas

--- src/screens/PinScreen.js
+++ src/screens/PinScreen.js
@@ -4,6 +4,7 @@ import {Ionicons} from "@expo/vector-icons";
 import pinsData from '../assets/data/pins';
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import {useNavigation, useRoute} from "@react-navigation/native";
+import {useNhostClient} from "@nhost/react";
 
 const PinScreen = () => {
     const [ratio, setRatio] = useState(1);
@@ -13,36 +14,59 @@ const PinScreen = () => {
     const route = useRoute();
     const pinId = route?.params?.id;
 
-    const pin = pinsData.find(pin => pin.id === pinId);
+    const nhost = useNhostClient();
+    // const pin = pinsData.find(pin => pin.id === pinId);
 
 
-    useEffect(() => {
-        if(pin?.image) {
-            // Here we are using the Image.getSize() method to get the width and height of the image and calculate the ratio.
-            Image.getSize(pin.image, (width, height) => setRatio(width / height));
-        }
-    }, [pin.image]);
+    // useEffect(() => {
+    //     if(pin?.image) {
+    //         // Here we are using the Image.getSize() method to get the width and height of the image and calculate the ratio.
+    //         Image.getSize(pin.image, (width, height) => setRatio(width / height));
+    //     }
+    // }, [pin.image]);
 
     const goBack = () => {
         // console.warn('Go back');
         navigation.goBack();
     }
 
+    const fetchPin = async () => {
+        const {data, error } = await nhost.graphql.request(`
+            query MyQuery {
+              pins_by_pk(id: "83494fd9-b87c-4def-ac33-6fa509d8c143") {
+                created_at
+                id
+                image
+                title
+                user_id
+                user {
+                  avatarUrl
+                  displayName
+                  id
+                }
+              }
+            }
+        `);
+        // console.log(data);
+    }
+
+    useEffect(() => { fetchPin() ; }, []);
+
     return (
         <SafeAreaView className="bg-black">
-            <View className="h-screen bg-black relative">
-                <View className="bg-black mt-4">
-                    <Image style={[styles.image, {borderTopLeftRadius: 35, borderTopRightRadius: 35, aspectRatio: ratio}]} source={{uri: pin.image}} />
-                    <TouchableOpacity onPress={goBack} className="absolute top-5 left-5" activeOpacity={0.7}>
-                        <Ionicons className="" name="chevron-back-outline" size={30} color="white" />
-                    </TouchableOpacity>
-                </View>
-                <View className="bg-gray-100 flex-1 items-center justify-start pt-4">
-                    <Text className="text-xl font-bold mx-8 tracking-wider" style={{lineHeight: 35}}>
-                        {pin.title}
-                    </Text>
-                </View>
-            </View>
+            {/*<View className="h-screen bg-black relative">*/}
+            {/*    <View className="bg-black mt-4">*/}
+            {/*        <Image style={[styles.image, {borderTopLeftRadius: 35, borderTopRightRadius: 35, aspectRatio: ratio}]} source={{uri: pin.image}} />*/}
+            {/*        <TouchableOpacity onPress={goBack} className="absolute top-5 left-5" activeOpacity={0.7}>*/}
+            {/*            <Ionicons className="" name="chevron-back-outline" size={30} color="white" />*/}
+            {/*        </TouchableOpacity>*/}
+            {/*    </View>*/}
+            {/*    <View className="bg-gray-100 flex-1 items-center justify-start pt-4">*/}
+            {/*        <Text className="text-xl font-bold mx-8 tracking-wider" style={{lineHeight: 35}}>*/}
+            {/*            {pin.title}*/}
+            {/*        </Text>*/}
+            {/*    </View>*/}
+            {/*</View>*/}
         </SafeAreaView>
     );
 };
