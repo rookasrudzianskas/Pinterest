0fbaaf7668dba70c653c43290e411aee31e95f6f
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 16:44:54 +0300

    Rokas

--- src/screens/Auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/Auth/SignUpScreen/SignUpScreen.tsx
@@ -7,112 +7,122 @@ import Colors from "../../../constants/Colors";
 import {useNhostClient} from "@nhost/react";
 
 const SignUpScreen = () => {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const nhost = useNhostClient();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const nhost = useNhostClient();
 
-  const onRegisterPressed = () => {
-    console.warn("Sign up");
-  };
+    const onRegisterPressed = async () => {
+        // console.warn("Sign up");
+        const result = await nhost.auth.signUp({
+            email,
+            password,
+            options: {
+                displayName: name,
+            }
+        });
+        console.log(result);
+        console.warn("User created");
+    };
 
-  const onSignInPress = () => {
-    // @ts-ignore
-    navigation.navigate("Sign in");
-  };
+    const onSignInPress = () => {
+        // @ts-ignore
+        navigation.navigate("Sign in");
+    };
 
-  const onTermsOfUsePressed = () => {
-    console.warn("onTermsOfUsePressed");
-  };
+    const onTermsOfUsePressed = () => {
+        console.warn("onTermsOfUsePressed");
+    };
 
-  const onPrivacyPressed = () => {
-    console.warn("onPrivacyPressed");
-  };
+    const onPrivacyPressed = () => {
+        console.warn("onPrivacyPressed");
+    };
 
-  return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={styles.root}>
-        <Text style={styles.title}>Create an account</Text>
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.root}>
+                <Text style={styles.title}>Create an account</Text>
 
-        <TextInput
-          value={name}
-          onChangeText={setName}
-          placeholder="Full name"
-          style={styles.input}
-        />
+                <TextInput
+                    value={name}
+                    onChangeText={setName}
+                    placeholder="Full name"
+                    style={styles.input}
+                />
 
-        <TextInput
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Email"
-          style={styles.input}
-        />
+                <TextInput
+                    autoCapitalize={"none"}
+                    value={email}
+                    onChangeText={setEmail}
+                    placeholder="Email"
+                    style={styles.input}
+                />
 
-        <TextInput
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-          secureTextEntry
-          style={styles.input}
-        />
+                <TextInput
+                    value={password}
+                    onChangeText={setPassword}
+                    placeholder="Password"
+                    secureTextEntry
+                    style={styles.input}
+                />
 
-        <CustomButton text="Register" onPress={onRegisterPressed} />
+                <CustomButton text="Register" onPress={onRegisterPressed} />
 
-        <Text style={styles.text}>
-          By registering, you confirm that you accept our{" "}
-          <Text style={styles.link} onPress={onTermsOfUsePressed}>
-            Terms of Use
-          </Text>{" "}
-          and{" "}
-          <Text style={styles.link} onPress={onPrivacyPressed}>
-            Privacy Policy
-          </Text>
-        </Text>
+                <Text style={styles.text}>
+                    By registering, you confirm that you accept our{" "}
+                    <Text style={styles.link} onPress={onTermsOfUsePressed}>
+                        Terms of Use
+                    </Text>{" "}
+                    and{" "}
+                    <Text style={styles.link} onPress={onPrivacyPressed}>
+                        Privacy Policy
+                    </Text>
+                </Text>
 
-        <SocialSignInButtons />
+                <SocialSignInButtons />
 
-        <CustomButton
-          text="Have an account? Sign in"
-          onPress={onSignInPress}
-          type="TERTIARY"
-        />
-      </View>
-    </ScrollView>
-  );
+                <CustomButton
+                    text="Have an account? Sign in"
+                    onPress={onSignInPress}
+                    type="TERTIARY"
+                />
+            </View>
+        </ScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
-  root: {
-    alignItems: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "black",
-    margin: 10,
-  },
-  text: {
-    color: "gray",
-    marginVertical: 10,
-  },
-  link: {
-    color: Colors.light.tint,
-  },
-  input: {
-    backgroundColor: "white",
-    width: "100%",
+    root: {
+        alignItems: "center",
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "black",
+        margin: 10,
+    },
+    text: {
+        color: "gray",
+        marginVertical: 10,
+    },
+    link: {
+        color: Colors.light.tint,
+    },
+    input: {
+        backgroundColor: "white",
+        width: "100%",
 
-    borderColor: "#e8e8e8",
-    borderWidth: 1,
-    borderRadius: 5,
+        borderColor: "#e8e8e8",
+        borderWidth: 1,
+        borderRadius: 5,
 
-    paddingHorizontal: 10,
-    marginVertical: 5,
-    height: 50,
-  },
+        paddingHorizontal: 10,
+        marginVertical: 5,
+        height: 50,
+    },
 });
 
 export default SignUpScreen;
