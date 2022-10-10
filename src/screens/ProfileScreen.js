41135d1f54512e97b2c1ed08f256ee68ad7363de
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 17:15:20 +0300

    Rokas

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,10 +3,11 @@ import PinsData from '../assets/data/pins';
 import { Text, View } from '../components/Themed';
 import {Feather} from "@expo/vector-icons";
 import MasonryList from "../components/MasonryList";
-import {useNhostClient} from "@nhost/react";
+import {useNhostClient, useSignOut} from "@nhost/react";
 
 export default function ProfileScreen() {
     const nhost = useNhostClient();
+    const {signOut} = useSignOut();
   return (
     <ScrollView className="bg-white pt-5 pb-4 w-[100%]">
       <View className="items-center">
@@ -17,7 +18,7 @@ export default function ProfileScreen() {
         <Text className="font-[600]">110 followers | 256 following</Text>
       </View>
       <View className="absolute top-0 right-1 flex-row items-center justify-end px-7 space-x-2">
-        <TouchableOpacity activeOpacity={0.7} onPress={() => nhost.auth.signOut()}>
+        <TouchableOpacity activeOpacity={0.7} onPress={signOut}>
           <Feather name="share" size={21} color="black" />
         </TouchableOpacity>
         <TouchableOpacity activeOpacity={0.7}>
