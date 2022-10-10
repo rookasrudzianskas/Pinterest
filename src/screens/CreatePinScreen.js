362505763338e76981aee6a6a6f1e1fc6a7761bb
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 14:28:15 +0300

    Rokas

--- src/screens/CreatePinScreen.js
+++ src/screens/CreatePinScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import {Button, Image, View, Platform, TextInput} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 const CreatePinScreen = () => {
@@ -8,13 +8,12 @@ const CreatePinScreen = () => {
     const pickImage = async () => {
         // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
+            quality: 0.6,
         });
 
-        console.log(result);
+        // console.log(result);
 
         if (!result.cancelled) {
             setImage(result.uri);
@@ -23,9 +22,12 @@ const CreatePinScreen = () => {
 
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} className="mx-5">
             <Button title="Pick an image from camera roll" onPress={pickImage} />
-            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            <View className={`${image && 'border border-blue-200 border-[2px] rounded-xl w-full'}`}>
+                {image && <Image source={{ uri: image }} style={{ width: "100%", aspectRatio: 1, borderRadius: 10 }} />}
+            </View>
+            <TextInput placeholder="Title..." className="py-2 bg-white w-full rounded-lg px-3 mt-5 border border-blue-200 border-[2px]"/>
         </View>
     );
 };
