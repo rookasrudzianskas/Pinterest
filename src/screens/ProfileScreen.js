70341cf0484eefd5ea31d245ca9d17b678e22d3b
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 12:49:19 +0300

    Rokas

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,18 +1,20 @@
-import {Image, StyleSheet, TouchableOpacity} from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import {Image, ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
+import PinsData from '../assets/data/pins';
 import { Text, View } from '../components/Themed';
 import {Feather} from "@expo/vector-icons";
+import MasonryList from "../components/MasonryList";
 
 export default function ProfileScreen() {
   return (
-    <View className="items-center justify-center flex-1">
-      <Image className="w-40 h-40 rounded-full" source={{uri: 'https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj'}} />
+    <ScrollView className="bg-white pt-5 pb-4 w-[100%]">
+      <View className="items-center">
+          <Image className="w-40 h-40 bg-blue-600 rounded-full align-center" source={{uri: 'https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj'}} />
+      </View>
       <View className="text-center items-center justify-center space-y-2 mt-4">
         <Text className='text-lg font-bold text-gray-800'>Rokas Rudzianskas</Text>
         <Text className="font-[600]">110 followers | 256 following</Text>
       </View>
-      <View className="absolute top-5 left-3 flex-row items-center justify-end px-7 space-x-2 w-full">
+      <View className="absolute top-0 right-1 flex-row items-center justify-end px-7 space-x-2">
         <TouchableOpacity activeOpacity={0.7}>
           <Feather name="share" size={21} color="black" />
         </TouchableOpacity>
@@ -20,7 +22,8 @@ export default function ProfileScreen() {
           <Feather name="more-horizontal" size={21} color="black" />
         </TouchableOpacity>
       </View>
-    </View>
+        <MasonryList pins={PinsData} />
+    </ScrollView>
   );
 }
 
