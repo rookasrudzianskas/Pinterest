25dad11e755aa978f4e5891ae98f18c382ea4359
Rokas <rookas.rudzenskas@gmail.com>
2022-10-10 16:21:48 +0300

    Rokas

--- src/screens/PinScreen.js
+++ src/screens/PinScreen.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, Image, TouchableOpacity, SafeAreaView, Alert} from 'react-native';
 import {Ionicons} from "@expo/vector-icons";
-import pinsData from '../assets/data/pins';
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import {useNavigation, useRoute} from "@react-navigation/native";
 import {useNhostClient} from "@nhost/react";
@@ -28,10 +27,10 @@ const PinScreen = () => {
         navigation.goBack();
     }
 
-    const fetchPin = async () => {
+    const fetchPin = async (pinId) => {
         const {data, error } = await nhost.graphql.request(`
             query MyQuery {
-              pins_by_pk(id: "83494fd9-b87c-4def-ac33-6fa509d8c143") {
+              pins_by_pk(id: "${pinId}") {
                 created_at
                 id
                 image
@@ -56,19 +55,19 @@ const PinScreen = () => {
 
     return (
         <SafeAreaView className="bg-black">
-            {/*<View className="h-screen bg-black relative">*/}
-            {/*    <View className="bg-black mt-4">*/}
-            {/*        <Image style={[styles.image, {borderTopLeftRadius: 35, borderTopRightRadius: 35, aspectRatio: ratio}]} source={{uri: pin.image}} />*/}
-            {/*        <TouchableOpacity onPress={goBack} className="absolute top-5 left-5" activeOpacity={0.7}>*/}
-            {/*            <Ionicons className="" name="chevron-back-outline" size={30} color="white" />*/}
-            {/*        </TouchableOpacity>*/}
-            {/*    </View>*/}
-            {/*    <View className="bg-gray-100 flex-1 items-center justify-start pt-4">*/}
-            {/*        <Text className="text-xl font-bold mx-8 tracking-wider" style={{lineHeight: 35}}>*/}
-            {/*            {pin.title}*/}
-            {/*        </Text>*/}
-            {/*    </View>*/}
-            {/*</View>*/}
+            <View className="h-screen bg-black relative">
+                <View className="bg-black mt-4">
+                    <Image style={[styles.image, {borderTopLeftRadius: 35, borderTopRightRadius: 35, aspectRatio: ratio}]} source={{uri: pin?.image}} />
+                    <TouchableOpacity onPress={goBack} className="absolute top-5 left-5" activeOpacity={0.7}>
+                        <Ionicons className="" name="chevron-back-outline" size={30} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View className="bg-gray-100 flex-1 items-center justify-start pt-4">
+                    <Text className="text-xl font-bold mx-8 tracking-wider" style={{lineHeight: 35}}>
+                        {pin?.title}
+                    </Text>
+                </View>
+            </View>
         </SafeAreaView>
     );
 };
